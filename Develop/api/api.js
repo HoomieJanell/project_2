23222af1b36b9f39f4924f25f45019fdea14f6c0
Justin Abreu <justinabreu92@gmail.com>
2020-02-04 13:59:01 -0600

    Can't get the server running, getting sequelize lib error

--- Develop/api/api.js
+++ Develop/api/api.js
@@ -1,13 +1,7 @@
-//require("dotenv").config()
-// var key = process.env.API_KEY;
-// console.log(key)
-
 var key = 'fcAeKESHSkTYxwedo5Gf1UAlThGC4Cq6';
-var url = "https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey="
+var url = "https://app.ticketmaster.com/discovery/v2/events.json?size=5&apikey="  
 
-  
-
-$.ajax({
+$.ajax({    
     type:"GET",
     url: url + key,
     async:true,
@@ -15,25 +9,38 @@ $.ajax({
     success: function(json) {
                 console.log(json); 
 
-                var events = json._embedded.events;
+                var allEvents = json._embedded.events;
 
-                for (var i = 0; i < events.length; i++){
-                                      
-                var eventName = events[i].name;
-                console.log(eventName)
+                for (var i = 0; i < allEvents.length; i++) {                                      
+                var eventName = allEvents[i].name;
+                console.log(eventName);
+                var eventGenre = allEvents[i].classifications[0].genre.name
+                console.log(eventGenre)
+                var eventDate = allEvents[i].dates.start.localDate
+                console.log(eventDate);
+                var eventLink = allEvents[i].url
+                console.log(eventLink);
 
-                var eventdate = events[i].dates.start.localDate
-                console.log(eventdate)
-
-                var eventTime = events[i].dates.start.localTime
-                console.log(eventTime)
-
-                var eventLink = events[i].url
-                console.log(eventLink)
-                }
+                    info = {
+                        event: eventName,
+                        genre: eventGenre,
+                        date: eventDate,
+                        ticketlink: eventLink
+                    };  
+                    
+                    $.post("/api/events", info),
+                        function(data, status) {
+                            console.log(data)
+                            console.log(status)
+                        }
+                }            
              },
     error: function(xhr, status, err) {
         console.log(err);
              }
-  });
+  })
+ 
+      
+  
+
 
